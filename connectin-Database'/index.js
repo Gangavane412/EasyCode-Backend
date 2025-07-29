9168a45f2166f1b56882e24908ad3775f72f52e7
Gangavane412 <gangawanepriyanka412@gmail.com>
2025-07-29 07:33:31 +0530

    adding meddleware  code

--- connectin-Database'/index.js
+++ connectin-Database'/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import logger from "./middleware/logger.js";
 
 const app = express();
 const port = process.env.PORT || 3000; // Use env first, fallback to 3000
@@ -8,6 +9,9 @@ const port = process.env.PORT || 3000; // Use env first, fallback to 3000
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use(logger);
 
 app.get("/health", (req, res) => {
   return res.json({
